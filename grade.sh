d41ffb0c3fd012f874a3eb5adf65dc6140976f23
Eric-Yan-742 <yuqiyan20040730@gmail.com>
2023-02-15 16:37:01 -0800

    grade.sh

--- grade.sh
+++ grade.sh
@@ -1,5 +1,29 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+rm -rf *.class
+rm -rf testResult.txt
 git clone $1 student-submission
 echo 'Finished cloning'
+if [[ -f "student-submission/ListExamples.java" ]]
+then
+    echo "File is found!"
+else
+    echo "Cannot find ListExamples.java! Exit code: 1"
+    exit 1
+fi
+cp student-submission/ListExamples.java .
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+if [[ $? -ne 0 ]]
+then
+    echo "Compile error"
+    exit 2
+fi
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > testResult.txt
+RESULT=`grep "failure" testResult.txt | wc -l`
+if [[ $RESULT -gt 0 ]]
+then
+    echo "Test Fail"
+else
+    echo "Test Pass"
+fi
